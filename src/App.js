72df10bf4a4b23fbf78a6ca1b71295f9ed5be5e5
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-10-14 19:29:44 -0600

    minor changes to imports

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SigninHome from './components/SigninHome.js';
 import Login from './components/Login';
 import {
   BrowserRouter as Router,
-  Switch, Route, NavLink, BrowserRouter
+  Switch, Route
 
 } from 'react-router-dom' 
 
